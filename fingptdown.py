ec580990a33b949405862423f82204274aaf0c89
ayushkanpur <imyourayush27@gmail.com>
2025-03-28 08:13:53 +0530

    Update fingptdownload.py

--- fingptdown.py
+++ fingptdown.py
@@ -1,12 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = "lxyuan/distilgpt2-finetuned-finance"
+# download the LLM model and save locally in folder FinChat_XS_local
+
+model_name = "oopere/FinChat-XS" # "lxyuan/distilgpt2-finetuned-finance"
 
 # Download and save model locally
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
 # Save to a local directory (e.g., "finGPT_tiny_local/")
-model.save_pretrained("distilgpt2_finetuned_finance_local")
-tokenizer.save_pretrained("distilgpt2_finetuned_finance_local")
+model.save_pretrained("FinChat_XS")
+tokenizer.save_pretrained("FinChat_XS_local")
 
